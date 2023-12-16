6a01395105cef6171aabc029b34bcb55b67de486
허 준성 <oksk476@naver.com>
2023-12-16 18:34:47 +0900

    VO에 lombok 라이브러리 사용하므로 getter, setter 메서드 삭제

--- src/main/java/com/kyobo/dto/UserVO.java
+++ src/main/java/com/kyobo/dto/UserVO.java
@@ -9,55 +9,6 @@ public class UserVO {
     private String login_id;
     private String login_pw;
     private String name;
-
-    public String getManage_no() {
-        return manage_no;
-    }
-
-    public void setManage_no(String manage_no) {
-        this.manage_no = manage_no;
-    }
-
-    public String getLogin_id() {
-        return login_id;
-    }
-
-    public void setLogin_id(String login_id) {
-        this.login_id = login_id;
-    }
-
-    public String getLogin_pw() {
-        return login_pw;
-    }
-
-    public void setLogin_pw(String login_pw) {
-        this.login_pw = login_pw;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     private String address;
     private String phone;
 }
